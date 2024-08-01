d603959e380c179d11ed3ae30b061c1b35f1f189
pohsengl <poh.seng.lim@intel.com>
2024-08-01 08:27:35 +0800

    [Temporary] Enable NPU to run wikitext-2-raw-v1 dataset for perplexity

--- python/llm/dev/benchmark/perplexity/run_wikitext.py
+++ python/llm/dev/benchmark/perplexity/run_wikitext.py
@@ -20,40 +20,66 @@
 import argparse
 import torch
 from tqdm import tqdm
+from datasets import concatenate_datasets, load_dataset
+from ipex_llm.utils.common import invalidInputError
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", required=True, type=str)
-parser.add_argument("--data_path", type=str, default='wikitext-2-raw-v1/wikitext-2-raw/wiki.test.raw')
+parser.add_argument("--dataset", type=str, default=None)
+parser.add_argument("--data_path", type=str, default=None)
 parser.add_argument("--chunk_size", type=int, default=512)
 parser.add_argument("--stride", type=int, default=0)
 parser.add_argument("--device", type=str, default="xpu")
 parser.add_argument("--precision", type=str, default="sym_int4")
 parser.add_argument("--use-cache", action="store_true")
+parser.add_argument("--max_length", type=int, default=None)
 args = parser.parse_args()
 
-if args.precision == "fp16":  # ipex fp16
-    from transformers import AutoModelForCausalLM
-    if "xpu" in args.device:
-        import intel_extension_for_pytorch as ipex
-    model = AutoModelForCausalLM.from_pretrained(args.model_path, use_cache=args.use_cache, trust_remote_code=True)
-    model = model.half()
-else:  # ipex-llm
-    from ipex_llm.transformers import AutoModelForCausalLM
-    model = AutoModelForCausalLM.from_pretrained(args.model_path, load_in_low_bit=args.precision,
-                                                 use_cache=args.use_cache, trust_remote_code=True)
-    model = model.half()
-model = model.to(args.device)
-model = model.eval()
+if args.device == "npu":
+    from ipex_llm.transformers.npu_model import AutoModelForCausalLM
+    model = AutoModelForCausalLM.from_pretrained(args.model_path, load_in_low_bit="sym_int4", trust_remote_code=True,
+                                                    use_cache=True)
+else:
+    if args.precision == "fp16":  # ipex fp16
+        from transformers import AutoModelForCausalLM
+        if "xpu" in args.device:
+            import intel_extension_for_pytorch as ipex
+        model = AutoModelForCausalLM.from_pretrained(args.model_path, use_cache=args.use_cache, trust_remote_code=True)
+        model = model.half()
+    else:  # ipex-llm
+        from ipex_llm.transformers import AutoModelForCausalLM
+        model = AutoModelForCausalLM.from_pretrained(args.model_path, load_in_low_bit=args.precision,
+                                                    use_cache=args.use_cache, trust_remote_code=True)
+        model = model.half()
+    model = model.to(args.device)
 
-with open(args.data_path, "rb") as f:
-    data = f.read()
+model = model.eval()
 
 from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-encodings = tokenizer(data.decode("utf-8").strip("\n"), return_tensors="pt")
 
-max_length = model.config.max_position_embeddings
+if args.dataset:
+    def parse_kwargs(kwstr):
+        kvpair = [item.split('=') for item in kwstr.split(',') if item != ""]
+        return {k:v for k, v in kvpair}
+    test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")["text"]
+    #test = load_dataset(**parse_kwargs(args.dataset), split="test")["text"]
+    encodings = tokenizer("\n\n".join(test), return_tensors="pt")
+elif args.data_path:
+    with open(args.data_path, "rb") as f:
+        data = f.read()
+    encodings = tokenizer(data.decode("utf-8").strip("\n"), return_tensors="pt")
+else:
+    raise invalidInputError(False, "Must specify either dataset or datapath.")
+
+if not args.max_length:
+    try:
+        max_length = model.config.max_position_embeddings
+    except:
+        max_length = model.config.seq_length  # max_length in config of chatglm is 'seq_length'
+else:
+    max_length = args.max_length
 stride = args.chunk_size if args.stride <= 0 else args.stride
 seq_len = encodings.input_ids.size(1)
 num_chunks = seq_len // stride
